98e1b0fa017df8722aea4d6df8dc50a1999a19d4
Khari Ollivierre <ollivierre.khari@gmail.com>
2017-02-17 19:19:26 -0500

    Triangle implementation

--- proj2_raytracer/src/librt/Triangle.cpp
+++ proj2_raytracer/src/librt/Triangle.cpp
@@ -1,7 +1,7 @@
 //----------------------------------------------------------------
 // Triangle.cpp
 // Copyright: Corey Toler-Franklin, University of Florida
-// 
+//
 // Triangle surface class
 // This class is represented by the surface enclosed by 3 points:m_a, m_b, m_c
 //----------------------------------------------------------------
@@ -15,7 +15,9 @@
 Triangle::Triangle(void)
         : Surface()
 {
-
+  m_a = STVector3(2, 1, 5);
+  m_b = STVector3(4, 0, 7);
+  m_c = STVector3(1, 4, 4);
 }
 
 
@@ -37,7 +39,7 @@ bool Triangle::IntersectionSolver(Ray ray, STVector3 A, STVector3 B, STVector3 C
 {
     LinearSolver L;
     bool bFoundSolution = false;
-    
+
     // TO DO: Proj2 raytracer
     //          - Solve for intersections.
     // 1. Use barycentric coordinates and linear equations to solve for intersections
@@ -65,13 +67,25 @@ bool Triangle::FindIntersection (Ray ray, Intersection *pIntersection)
     //          - Find Intersections.
     // 1. Find intersections with this object along the Ray ray
     //    Use barycentric coordinates and linear equations
-    // 2. Store the results of the intersection 
+    // 2. Store the results of the intersection
     // 3. If found return true, otherwise, return false
     // NOTE: The Intersection pIntersection should store:
     // hit point, surface normal, the time t of the ray at the hit point
     // and the surface being hit
     //------------------------------------------------
+    STVector3 normal = ComputeNormalVector()
+    float d = STVector3::Dot(normal, m_a);
+    float t = -(STVector3::Dot(normal, ray.Origin() + d)/STVector3::Dot(normal, ray.Direction());
+    if (t < 0) return false;
+    STVector3 p = ray.Origin() + (t * ray.Direction());
 
+    pIntersection->setDistanceSqu(t);
+    //std::cout << "Intersection distance added" << std::endl;
+    pIntersection->point = p;
+    //std::cout << "Intersection point added" << std::endl;
+    pIntersection->normal = normal;
+    //std::cout << "Intersection normal added" << std::endl;
+    pIntersection->surface = this;
     //------------------------------------------------------
 
     return(bFound);
@@ -89,7 +103,8 @@ STVector3 Triangle::ComputeNormalVector(void)
     // 1. Compute the surface surface normal to the
     // plane whose points are m_a, m_b, and m_c
     //------------------------------------------------
-
+    normal = STVector3::Cross((m_a - m_b) (m_b - m_c));
+    normal.Normalize();
     //---------------------------------------------------
 
     return(normal);
@@ -101,6 +116,6 @@ STVector3 Triangle::ComputeNormalVector(void)
 // 1. Add any object specific properties you need
 //    to create your special effects (e.g. specularity, transparency...)
 // 2. Remember to declare these in your .h file
-// 
+//
 //---------------------------------------------------------
 //---------------------------------------------------------
