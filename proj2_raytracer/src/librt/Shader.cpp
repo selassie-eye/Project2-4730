3da18f389b6eefa76ee9eee983bc27cef8decb6a
Khari Ollivierre <ollivierre.khari@gmail.com>
2017-02-17 20:51:51 -0500

    Phong shading

--- proj2_raytracer/src/librt/Shader.cpp
+++ proj2_raytracer/src/librt/Shader.cpp
@@ -97,9 +97,9 @@ RGBR_f Shader::Phong(Intersection *pIntersection, STVector3 *lightDirection)
     STVector3 lightDir = *lightDirection;
     RGBR_f color;
     float albedo = 1;
-    float kd = 100;
-    float ks = 100;
-    float n = 5;
+    float kd = 1;
+    float ks = 1;
+    float n = 1;
 
     // TO DO: Proj2 raytracer
     //          - Add Phong shading.
